67837100d9090c61d673bb6a8eefb5a956e9b2d0
lclup <lcl3219371510@gmail.com>
2021-12-17 15:51:19 +0800

    first commit

---
+++ src/com/colin/test.java
@@ -0,0 +1,15 @@
+package com.colin;
+
+/**
+ * @author lcl
+ * @create 2021/12/17 15:43
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("zhangtingting ");
+        System.out.println("luohuishuang ");
+        System.out.println("lichaolong");
+        System.out.println("songqingbing");
+    }
+
+}
